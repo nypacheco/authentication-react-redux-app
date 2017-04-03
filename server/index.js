fac6365dfd6a29e9debd0a7feec35fc3c53d7848
Nypacheco <nathalia.yp19@gmail.com>
2017-04-03 15:46:21 -0300

    #05

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.use(webpackMiddleware(compiler, {
     publicPath: webpackConfig.output.publicPath,
     noInfo: true
 }));
+
 app.use(webpackHotMiddleware(compiler));
 
 app.get("/*", (req, res) => {
