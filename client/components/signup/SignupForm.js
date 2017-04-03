f45e327472a657e2e6c5e4abfee4939b049ed4cb
Nypacheco <nathalia.yp19@gmail.com>
2017-04-03 18:28:58 -0300

    #07

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames'
 
 class SignupForm extends React.Component {
     constructor(props){
@@ -7,8 +8,11 @@ class SignupForm extends React.Component {
             username: '',
             email: '',
             password: '',
-            passwordConfirmation: ''
+            passwordConfirmation: '',
+            errors: {},
+            isLoading: false
         };
+
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
     };
@@ -16,14 +20,22 @@ class SignupForm extends React.Component {
         this.setState({[e.target.name]: e.target.value});
     };
     onSubmit(e){
+        this.setState({ errors: {}, isLoading: true});
         e.preventDefault();
-        this.props.userSignupRequest(this.state);
+        this.props.userSignupRequest(this.state).then(
+            () => {},
+            (data) => {
+                this.setState({ errors: data.response.data, isLoading: false })
+            }
+        );
     };
     render(){
+        const { errors } = this.state;
+
         return (
            <form onSubmit={this.onSubmit}>
                <h1>Join our community!</h1>
-               <div className="form-group">
+               <div className={classNames("form-group", {'has-error': errors.username})}>
                    <label className="control-label">Username</label>
                    <input
                        value={this.state.username}
@@ -31,8 +43,9 @@ class SignupForm extends React.Component {
                        type="text"
                        name="username"
                        className="form-control"/>
+                   {errors.username && <span className="help-block">{errors.username}</span>}
                </div>
-               <div className="form-group">
+               <div className={classNames("form-group", {'has-error': errors.email})}>
                    <label className="control-label">Email</label>
                    <input
                        value={this.state.email}
@@ -40,8 +53,9 @@ class SignupForm extends React.Component {
                        type="text"
                        name="email"
                        className="form-control"/>
+                   {errors.email && <span className="help-block">{errors.email}</span>}
                </div>
-               <div className="form-group">
+               <div className={classNames("form-group", {'has-error': errors.password})}>
                    <label className="control-label">Password</label>
                    <input
                        value={this.state.password}
@@ -49,8 +63,9 @@ class SignupForm extends React.Component {
                        type="password"
                        name="password"
                        className="form-control"/>
+                   {errors.password && <span className="help-block">{errors.password}</span>}
                </div>
-               <div className="form-group">
+               <div className={classNames("form-group", {'has-error': errors.passwordConfirmation})}>
                    <label className="control-label">Password Confirmation</label>
                    <input
                        value={this.state.passwordConfirmation}
@@ -58,9 +73,10 @@ class SignupForm extends React.Component {
                        type="password"
                        name="passwordConfirmation"
                        className="form-control"/>
+                   {errors.passwordConfirmation && <span className="help-block">{errors.passwordConfirmation}</span>}
                </div>
                <div className="form-group">
-                   <button className="btn btn-primary btn-lg">
+                   <button disabled={this.state.isLoading} className="btn btn-primary btn-lg">
                        Signup
                    </button>
                </div>
---
+++ server/routes/users.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import Validator from 'validator';
+import isEmpty from 'lodash/isEmpty';
+
+let router = express.Router();
+
+function validateInput(data){
+    let errors = {};
+
+    if(Validator.isEmpty(data.username)){
+        errors.username = 'Username is required';
+    }
+    if(Validator.isEmpty(data.email)){
+        errors.email = 'Email is required';
+    }
+    if(!Validator.isEmail(data.email)){
+        errors.email = 'Email is invalid';
+    }
+    if(Validator.isEmpty(data.password)){
+        errors.password = 'Password is required';
+    }
+    if(Validator.isEmpty(data.passwordConfirmation)){
+        errors.passwordConfirmation = 'Password confirmation is required';
+    }
+    if(!Validator.equals(data.password, data.passwordConfirmation)){
+        errors.passwordConfirmation = 'Passwords must match';
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+
+router.post('/', (req, res) => {
+    const {errors, isValid} = validateInput(req.body);
+
+    if(!isValid){
+        res.status(404).json(errors);
+    }
+});
+
+export default router;
