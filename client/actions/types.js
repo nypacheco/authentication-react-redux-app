3129b988b6cda4a68cfa3a8b952558b78cedabc3
Nypacheco <nathalia.yp19@gmail.com>
2017-04-03 22:26:21 -0300

    #10

---
+++ client/actions/types.js
@@ -0,0 +1,2 @@
+const ADD_FLASH_MESSAGE = 'ADD_FLASH_MESSAGE';
+export default ADD_FLASH_MESSAGE;
--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -40,6 +40,10 @@ class SignupForm extends React.Component {
             this.setState({errors: {}, isLoading: true});
             this.props.userSignupRequest(this.state).then(
                 () => {
+                    this.props.addFlashMessage({
+                        type: 'success',
+                        message: 'You signed up successfully. Welcome!'
+                    });
                     browserHistory.push('/');
                 },
                 (data) => {
@@ -94,7 +98,8 @@ class SignupForm extends React.Component {
 };
 
 SignupForm.propTypes = {
-    userSignupRequest: React.PropTypes.func.isRequired
+    userSignupRequest: React.PropTypes.func.isRequired,
+    addFlashMessage: React.PropTypes.func.isRequired
 };
 
 export default SignupForm;
